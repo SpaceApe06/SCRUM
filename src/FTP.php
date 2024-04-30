333a3461877c939f02737c31473376a65a8207da
Deadguana <alexandermofre@gmail.com>
2024-04-30 13:05:40 +0200

    ny ip

--- src/FTP.php
+++ src/FTP.php
@@ -1,5 +1,5 @@
 <?php
-$ftp_server = "172.25.1.101"; //FTP server addressen
+$ftp_server = "172.25.1.103"; //FTP server addressen
 $ftp_user = "ftpuser"; // Spesifiser brukenavnet til FTP server
 $ftp_pass = "IMKuben1337!"; // Spesifiserer password til FTP server
 $file = "/srv/ftp/wolfenstein"; // Spesifiserer filen som skal lastes ned
